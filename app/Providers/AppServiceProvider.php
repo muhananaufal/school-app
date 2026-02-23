dce7e0e6a34bd9c8e9f715b8f11fbb0c042b8c3d
muhananaufal <muhananaufal8@gmail.com>
2026-02-23 13:34:02 +0700

    feat: bind repository interfaces in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,12 @@
 
 namespace App\Providers;
 
+use App\Repositories\ClassroomRepository;
+use App\Repositories\Contracts\ClassroomRepositoryInterface;
+use App\Repositories\Contracts\StudentRepositoryInterface;
+use App\Repositories\Contracts\TeacherRepositoryInterface;
+use App\Repositories\StudentRepository;
+use App\Repositories\TeacherRepository;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
@@ -12,9 +18,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(ClassroomRepositoryInterface::class, ClassroomRepository::class);
+        $this->app->bind(TeacherRepositoryInterface::class, TeacherRepository::class);
+        $this->app->bind(StudentRepositoryInterface::class, StudentRepository::class);
     }
-
     /**
      * Bootstrap any application services.
      */
